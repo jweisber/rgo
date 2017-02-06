afb640bf922c39cf7baf54a23d20f445ac7f8012
Jonathan Weisberg <jonathan.weisberg@utoronto.ca>
2017-02-06 10:39:02 -0500

    New post: Reviewer 2, and update to rgo_functions.R

--- rgo_functions.R
+++ rgo_functions.R
@@ -1,7 +1,7 @@
 library(RMySQL)
 
 load_table <- function(tbl_name) {
-  con <- dbConnect(dbConnect(MySQL(), default.file = "~/.mylogin.cnf", group = "ergonaut_dev", user = NULL, password = NULL))
+  con <- dbConnect(MySQL(), default.file = "~/.mylogin.cnf", group = "ergonaut_dev", user = NULL, password = NULL)
   
   query_string <- paste("SELECT * FROM ", tbl_name)
   df <- dbGetQuery(con, query_string)
@@ -17,3 +17,15 @@ msq.test <- function(fac1, fac2) {
 }
 
 week.abb = c("Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat")
+
+test.report <- function(test, type) {
+  str <- NULL
+  if (type == "chi.sq") {
+    str <- paste("$\\chi^2$(", test$parameter,
+               ", *N* = ", sum(test$observed),
+               ") = ", round(test$statistic, 2),
+               ", *p* = ", round(test$p.value, 2),
+               sep = "")
+  }
+  return(str)
+}
